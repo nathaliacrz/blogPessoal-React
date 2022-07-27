0fc462c4637e7dd93da5206885cf3f5c4ecfaaf1
nathaliacrz <nathalias161@gmail.com>
2022-07-27 11:38:34 -0300

    finalizando instalção de dependencias

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+        <h1 className="titulo">Web Development</h1>
+        <img className="titulo-imagem" src="https://cdn-icons-png.flaticon.com/512/6486/6486032.png" alt=""/>
+    </>
+  )      
+}
+
+export default Home
