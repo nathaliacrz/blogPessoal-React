9f57aa885c2b7463eff01a443f177cddb507775d
nathaliacrz <nathalias161@gmail.com>
2022-08-02 16:09:41 -0300

    models e service

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-<link rel="stylesheet" href="./Navbar.css"></link>
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+            <Route path="/cadastro" element={<CadastroUsuario />} /> 
           </Routes>
         </div>
         <Footer />
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,34 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Box } from '@mui/material';
 import { Grid, TextField, Typography, Button } from '@material-ui/core';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
 
 import './Login.css';
+import { error } from 'console';
 
 function Login() {
+
+    let history = useNavigate();
+
+    const[token, setToken] = useLocalStorage('token'); 
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '', 
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+        
+        useEffect(() => {
+            if(token !== ''){
+                history('/home')
+            }
+        }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+            e.preventDefault();
+
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+                alert('Usuário logado com sucesso!!')
+            } catch(error){
+                alert('Dados do usuário do inconsistentes. Erro ao logar!!')
+            }
+        }
+
     return (
         <Grid className='box-form' container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
-                <Box  paddingX={20}>
-                    <form>
+                <Box paddingX={20}>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className="textos">
                             Entrar
                         </Typography>
-                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary' style={{ borderColor: "white", borderRadius: "20px", backgroundColor: "#a333e2", color: "white" }}>
                                     Logar
-                                </Button>
-                            </Link>
+                                </Button>                          
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'>Não tem uma conta?</Typography>
                         </Box>
+                        <Link to='/cadastro'>
                             <Typography variant='subtitle1' gutterBottom align='center' className="textos">Cadastre-se</Typography>
+                        </Link>
                     </Box>
                 </Box>
             </Grid>
