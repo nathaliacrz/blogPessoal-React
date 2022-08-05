96b4d92f3e64b8bc6944f7893e508d6850e73ad6
nathaliacrz <nathalias161@gmail.com>
2022-08-05 19:10:01 -0300

    crud

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import Login from './pages/login/Login';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import ListaTema from './components/temas/listaTema/ListaTema';
 import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
@@ -15,13 +19,21 @@ function App() {
       <Router>
         <Navbar />
         <div style={{ minHeight: '100vh' }}>
-          <Routes>  
+          <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<CadastroUsuario />} /> 
+            <Route path="/cadastro" element={<CadastroUsuario />} />
             <Route path="/temas" element={<ListaTema />} />
             <Route path="/posts" element={<ListaPostagem />} />
+            
+            <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+            <Route path="/formularioTema" element={<CadastroTema />} />
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
           </Routes>
         </div>
         <Footer />
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -2,21 +2,31 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { Toolbar, AppBar, Typography } from '@material-ui/core';
 import HomeIcon from '@mui/icons-material/Home';
+import { useNavigate } from 'react-router-dom';
+import useLocalStorage from 'react-use-localstorage';
 import { Link } from 'react-router-dom';
 
 import './Navbar.css'
 
 export default function Navbar() {
+    const [token, setToken] = useLocalStorage('token');
+    let history = useNavigate();
+
+    function goLogout() {
+        setToken('')
+        alert("Usuário deslogado.")
+        history('/login')
+    }
 
     return (
         <>
             <AppBar className='cor'>
                 <Toolbar variant="dense" style={{ display: "flex", justifyContent: "space-between" }}>
                     <Box mx={1} style={{ cursor: "pointer", display: 'flex', gap: '20px', alignItems: 'center' }}>
-                        <Link to='/home' className='cursor' >                         
-                                <HomeIcon />                           
+                        <Link to='/home' className='cursor' >
+                            <HomeIcon />
                         </Link>
-                        
+
                         <Typography color="inherit" font-size="5px">
                             Blog Pessoal
                         </Typography>
@@ -40,19 +50,21 @@ export default function Navbar() {
                             </Box>
                         </Link>
 
-                        <Box className='cursor' mx={1}>
-                            <Typography font-size="5px" color="inherit">
-                                Cadastrar tema
-                            </Typography>
-                        </Box>
-
-                        <Link to='/login' className='text-decorator-none'>
+                        <Link to='/formularioTema' className='text-decorator-none'>
                             <Box className='cursor' mx={1}>
+                                <Typography font-size="5px" color="inherit">
+                                    Cadastrar tema
+                                </Typography>
+                            </Box>
+                        </Link>
+
+                        
+                            <Box className='cursor' mx={1} onClick={goLogout}>
                                 <Typography font-size="5px" color="inherit">
                                     Logout
                                 </Typography>
                             </Box>
-                        </Link>
+                       
                     </Box>
 
                 </Toolbar>
