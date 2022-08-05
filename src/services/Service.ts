0f0221b3c0dd4e73894f08017bddfadac5e548d8
nathaliacrz <nathalias161@gmail.com>
2022-08-05 19:09:22 -0300

    componentes lista tema e postagem

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -13,3 +13,9 @@ export const api = axios.create({
         const resposta = await api.post(url, dados)
         setDado(resposta.data.token)
     }
+
+    // header: só pode realizar a postagem se o usuário estiver logado
+    export const busca = async (url: any, setDado: any, header: any) => {
+        const resposta = await api.get(url, header)
+        setDado(resposta.data)
+    }
