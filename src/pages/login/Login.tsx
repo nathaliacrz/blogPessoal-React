20c0386c34d799600e73c1d2f29e38afbd4b1675
nathaliacrz <nathalias161@gmail.com>
2022-08-24 14:51:28 -0300

    atualizando login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, ChangeEvent } from 'react';
-import { Box } from '@mui/material';
+import { Box, InputAdornment } from '@mui/material';
 import { Grid, TextField, Typography, Button } from '@material-ui/core';
 import { Link, useNavigate } from 'react-router-dom';
 import { login } from '../../services/Service';
@@ -9,6 +9,7 @@ import { useDispatch } from 'react-redux';
 import { addToken } from '../../store/tokens/actions';
 import './Login.css';
 import { toast } from 'react-toastify';
+import { AccountCircle, VpnKey } from '@material-ui/icons';
 
 
 function Login() {
@@ -70,14 +71,49 @@ function Login() {
     return (
         <Grid className='box-form' container direction='row' justifyContent='center' alignItems='center'>
             <Grid xs={12} className="imagem centralizar" justifyContent='center' >
-            <Grid alignItems='center' xs={4} justifyContent='center' >
+            <Grid alignItems='center' xs={4} justifyContent='center' className="centralizar">
                 <Box paddingX={20} className='box box-principal'>
                     <form onSubmit={onSubmit} className='box'>
                         <Typography style={{ color: 'white' }} variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className="textos">
                             LOGIN
                         </Typography>
-                        <TextField style={{ backgroundColor: 'white' }} value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='Email' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField style={{ backgroundColor: 'white' }} value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField style={{ backgroundColor: 'white' }} 
+                        value={userLogin.usuario}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                         id='usuario' 
+                         label='Email' 
+                         variant='outlined' 
+                         name='usuario' 
+                         margin='normal' 
+                         fullWidth 
+                         required
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <AccountCircle />
+                                        </InputAdornment>
+                                    ),
+                                }}/>
+
+                        <TextField style={{ backgroundColor: 'white' }}
+                         value={userLogin.senha} 
+                         onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)} 
+                         id='senha' 
+                         label='Senha' 
+                         variant='outlined' 
+                         name='senha' 
+                         margin='normal' 
+                         type='password' 
+                         fullWidth 
+                         required
+                                inputProps={{minLength: 8,maxLength: 50}}
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <VpnKey />
+                                        </InputAdornment>
+                                    ),
+                                }}/>
                         <Box marginTop={2} textAlign='center'>
                             <Button type='submit' variant='contained' color='primary' className='botao'>
                                 Login
